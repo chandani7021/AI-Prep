1b90d770c640a21693b6bf19d12016924fa68d27
chandani7021 <chandanimaurya56@gmail.com>
2026-02-26 07:21:55 +0530

    feat: implemented auth storeage

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -1,6 +1,6 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore } from "firebase-admin/firestore";
 
 const initFirebaseAdmin = () => {
 	const apps = getApps();
@@ -8,8 +8,8 @@ const initFirebaseAdmin = () => {
 	if (!apps.length) {
 		initializeApp({
 			credential: cert({
-				projectId: process.env.FIREBASE_PROJECT_ID,
-				privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+				projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+				privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
 				clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 			}),
 		});
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,26 +1,31 @@
+"use server";
+
 import { auth, db } from "@/firebase/admin";
-import { doc, getDoc, setDoc } from "firebase/firestore";
 import { cookies } from "next/headers";
 
-const ONE_WEEK = 60 * 60 * 24 * 7 ; // 7 days in milliseconds
+const ONE_WEEK = 60 * 60 * 24 * 7; // 7 days in milliseconds
 
 export async function signup(params: SignUpParams) {
 	const { uid, name, email } = params;
 
 	try {
 		// const userRecord = await db.collection('users').doc(uid).get()
-		const userRecord = await getDoc(doc(db, "users", uid));
-		if (userRecord.exists()) {
+		const userRecord = await db.collection("users").doc(uid).get();
+		if (userRecord.exists) {
 			return {
 				success: false,
 				message: "Email already in use. Please sign in instead.",
 			};
 		}
-		await setDoc(doc(db, "users", uid), {
+		await db.collection("users").doc(uid).set({
 			name,
 			email,
 			createdAt: new Date(),
 		});
+		return {
+			success: true,
+			message: "User signed up successfully, Please sign in.",
+		};
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	} catch (error: any) {
 		console.error("Error signing up user:", error);
@@ -39,40 +44,73 @@ export async function signup(params: SignUpParams) {
 
 
 export async function signin(params: SignInParams) {
-    const { email, idToken } = params;  
+	const { email, idToken } = params;
 
-    try {
-        const userRecord = await auth.getUserByEmail(email);
+	try {
+		const userRecord = await auth.getUserByEmail(email);
 
-        if (!userRecord) {
-            return {
-                success: false,
-                message: "No user found with this email. Please sign up first.",
-            };
-        }
+		if (!userRecord) {
+			return {
+				success: false,
+				message: "No user found with this email. Please sign up first.",
+			};
+		}
 
-        await setSessionCookie(idToken);
-    }
-    catch (error) {
-        console.error("Error signing in user:", error);
-        return {
-            success: false,
-            message: "An error occurred during sign in. Please try again.",
-        };
-    }
+		await setSessionCookie(idToken);
+	}
+	catch (error) {
+		console.error("Error signing in user:", error);
+		return {
+			success: false,
+			message: "An error occurred during sign in. Please try again.",
+		};
+	}
 }
 
 
 export async function setSessionCookie(idToken: string) {
-    const cookieStore = await cookies();
+	const cookieStore = await cookies();
 
-    const sessionCookie = await auth.createSessionCookie(idToken, { expiresIn: 60 * 60 * 24 * 7 * 1000 }); // 7 days
+	const sessionCookie = await auth.createSessionCookie(idToken, { expiresIn: 60 * 60 * 24 * 7 * 1000 }); // 7 days
 
-    cookieStore.set("session", sessionCookie, {
-        maxAge : ONE_WEEK,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        path: "/",
-        sameSite: "lax",
-    })
+	cookieStore.set("session", sessionCookie, {
+		maxAge: ONE_WEEK,
+		httpOnly: true,
+		secure: process.env.NODE_ENV === "production",
+		path: "/",
+		sameSite: "lax",
+	})
+}
+
+
+export async function getCurrentUser():Promise<User | null> {
+	const cookieStore = await cookies();
+	const sessionCookie = cookieStore.get("session")?.value;
+
+	if (!sessionCookie) {
+		return null;
+	}
+
+	try {
+		const decodedClaims = await auth.verifySessionCookie(sessionCookie , true);
+		const userRecord = await db.collection("users").doc(decodedClaims.uid).get();
+
+		if (!userRecord.exists) {
+			return null;
+		}
+		return {
+			...userRecord.data(),
+			id: userRecord.id,
+		} as User;
+
+	} catch (error) {
+		console.error("Error getting current user:", error);
+		return null;
+	}
+}
+
+
+export async function isAuthenticated() {
+	const user = await getCurrentUser();
+	return !!user;
 }
